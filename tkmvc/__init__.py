54f739c1de6d7749c7796f789b282b70ad90436f
Dan Huckson <danhuckson@hotmail.com>
2018-09-30 16:07:17 -0400

    Updated base code.

--- tkmvc/__init__.py
+++ tkmvc/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for TkMVC."""
 
+from tkmvc.tkmvc import App
+
 __author__ = """Dan Huckson"""
 __email__ = 'danhuckson@hotmail.com'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
